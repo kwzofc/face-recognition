26a4ac906885b6a248edd139e82bc8d0b0b62ce2
kwzofc <kwzofc@live.com>
2021-08-04 01:12:00 +0700

    update working with backend

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,13 +5,14 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Gift it a try.'}
+        {'This will detect faces in your pictures. Give it a try.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input 
             className='f4 pa2 w-70 center' 
             type='tex' 
+            placeholder='Insert your picture link here...' 
             onChange = {onInputChange}
           />
           <button 
