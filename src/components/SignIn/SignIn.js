f5b9ea9fb38cc01f801f37f208ce0ee066e80c87
Watcharapon Wangwuttikorn <45683322+kwzofc@users.noreply.github.com>
2021-08-05 17:01:13 +0700

    Update SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://nameless-sierra-41157.herokuapp.com/signin', {
+    fetch('backend url/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
