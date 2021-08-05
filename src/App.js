f6bef599c7e83662b11eb18bd570dff6228a2d4d
Watcharapon Wangwuttikorn <45683322+kwzofc@users.noreply.github.com>
2021-08-05 17:34:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('backend url', {
+    fetch('backend url/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('backend url', {
+          fetch('backend url/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
